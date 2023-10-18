8c173c6168bd2bb435d9f393ab933fb6620b78ca
Henri Hämäläinen <Henri.Hamalainen@student.lab.fi>
2023-10-18 10:14:44 +0300

    testien lisäyksiä

--- src/app/feedback/feedback.component.spec.ts
+++ src/app/feedback/feedback.component.spec.ts
@@ -1,17 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FeedbackComponent } from './feedback.component';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+
+// RUN TEST
+// ng test --include=src/app/feedback/feedback.component.spec.ts
 
 describe('FeedbackComponent', () => {
   let component: FeedbackComponent;
   let fixture: ComponentFixture<FeedbackComponent>;
+  let router: Router;
+  const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule.withRoutes],
       declarations: [ FeedbackComponent ]
     })
     .compileComponents();
 
+    router = TestBed.inject(Router);
     fixture = TestBed.createComponent(FeedbackComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +27,10 @@ describe('FeedbackComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('cancel-button should navigate back to homepage', () => {
+    component.cancel();
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/home']);
   });
 });
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,10 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { Router } from '@angular/router';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -19,5 +20,10 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('button navigates to feedbackpage', () => {
+    component.showFeedbackPage();
+    expect(routerSpy.navigate).toHaveBeenCalledWith(['/feedback']);
   });
 });
